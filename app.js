7794d39a80c1fa771c7b7bdba024fbe653d88caf
Rahul Sharma <hubstackrealm@gmail.com>
2026-07-21 15:46:36 +0530

    Add copyWithin examples for inventory array
    Using copyWithin() again to copy the last 3 products and placing them in the start of array

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ let inventory = [
     "Printer",
     "Scanner"
 ];
+let inventoryNew = [...inventory];                               //using the spread parameter to create copy of array
+let inventoryNew1 = [...inventory];                               //using the spread parameter to create copy of array
+
 
 // 1. Replacing the last two products with the first two products
-let inventoryNew = [...inventory];                               //using the spread parameter to create new array
 console.log(inventoryNew.copyWithin(4, 0, 2));                  //Using the copyWithin() method to print the updated list
+
+// 2. Copying the last three products to the begining of the inventory
+console.log(inventoryNew1.copyWithin(0, 3, 6));
